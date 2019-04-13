c21d7f5607c855522be9cca4e563e1cc9115ed8c
deno101 <dennizwarui@gmail.com>
2019-04-13 21:49:58 +0300

    Finalize and implement all functions for two player game

--- Tic-Tac-Toe__Deno101/window.py
+++ Tic-Tac-Toe__Deno101/window.py
@@ -5,10 +5,26 @@ txt = ''
 turn = 'X'
 btn = {}
 whiteimg = ''
+xplays = set([])
+oplays = set([])
+iswinner = False
 
 
 class Processing:
-    pass
+    def check_winner(self):
+        global iswinner, xplays, oplays, txt, turn
+        inhouseturn = "X" if turn == "O" else "O"
+        data = xplays if inhouseturn == 'X' else oplays
+        if not len(data) >= 3:
+            return
+        wincond = [{1, 2, 3}, {4, 5, 6}, {7, 8, 9}, {1, 4, 7}, {2, 5, 8}, {3, 6, 9}, {1, 5, 9}, {3, 5, 7}]
+        for x in wincond:
+            if x <= data:
+                iswinner = True
+                txt.config(text="%s wins" % inhouseturn)
+                for s in btn:
+                    btn[s].__state__ = False
+                break
 
 
 class App:
@@ -43,7 +59,7 @@ class Frame1(tk.Frame):
         self.columnconfigure(0, weight=1)
 
 
-class Frame2up:
+class Frame2up(Processing):
     global btn
 
     def main(self, parent, controller):
@@ -60,7 +76,8 @@ class Frame2up:
         for x in range(0, 9, 1):
             x = tk.Label(self.con, image=self.uimg)
             x.grid(row=rowg, column=colg, padx=2, pady=2)
-            x.__name__ = 'Btn-%s' % str(i)
+            x.__name__ = i
+            x.__state__ = True
             x.bind("<Button-1>", self.clickevent)
             btn[x.__name__] = x
             i += 1
@@ -73,16 +90,24 @@ class Frame2up:
         return self.con
 
     def clickevent(self, event):
-        global turn, txt
-        if turn == 'X'"":
+        global turn, txt, xplays, oplays, iswinner
+
+        if event.widget.__state__:
+            event.widget.__state__ = False
+        else:
+            return
+        if turn == 'X' and not iswinner:
+            xplays.add(event.widget.__name__)
             turn = 'O'
             img = self.ximg
-        elif turn == 'O':
+            txt.config(text="'%s' turn" % turn)
+        elif turn == 'O' and not iswinner:
+            oplays.add(event.widget.__name__)
             turn = 'X'
             img = self.oimg
+            txt.config(text="'%s' turn" % turn)
         event.widget.config(image=img)
-        txt.config(text="'%s' turn" % turn)
-
+        self.check_winner()
 
 class Frame2down:
     def main(self, parent, controller):
@@ -91,25 +116,30 @@ class Frame2down:
         reset = tk.Button(self.con, text='reset', command=lambda: self.reset())
         reset.grid(row=0, column=0, columnspan=2, pady=10)
         self.con.pack()
-        txt = tk.Label(self.con, text='')
+        txt = tk.Label(self.con, text="'X' turn")
         txt.grid(row=1, column=0, columnspan=3, padx=10)
         return self.con
 
+
     def reset(self):
-        global btn, whiteimg, txt, turn
+        global btn, whiteimg, txt, turn, xplays, oplays, iswinner
 
         for x in btn:
+            xplays = set([])
+            oplays = set([])
             btn[x].config(image=whiteimg)
+            iswinner = False
+            for s in btn:
+                btn[s].__state__ = True
             txt.config(text="'X' turn")
             turn = "X"
-
 
 
 class Frame2(Frame2up, Frame2down, tk.Frame):
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
-        f1 = Frame2up().main(self, controller)
-        f2 = Frame2down().main(self, controller)
+        Frame2up().main(self, controller)
+        Frame2down().main(self, controller)
 
 
 App()
